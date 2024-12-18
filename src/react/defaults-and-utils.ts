90231153289f09c7639e40caaf8ed872d716ddc7
Brett Holcomb <ready.set.get.brett@gmail.com>
2024-12-18 10:18:14 -0800

    Update defaults-and-utils.ts

--- src/react/defaults-and-utils.ts
+++ src/react/defaults-and-utils.ts
@@ -1,21 +1,21 @@
-export const defaultModifier = { multiplier: 1, sensitivity: 1 };
-export const defaultModifiers = {
-  default: defaultModifier,
-  ctrlKey: defaultModifier,
-  shiftKey: {
-    multiplier: 10,
-    sensitivity: 0.5,
-  },
-  metaKey: defaultModifier,
-  altKey: defaultModifier,
+export interface Modifier {
+  multiplier: number;
+  sensitivity: number;
+}
+
+export type Modifiers = {
+  [key in "default" | "shiftKey" | "ctrlKey" | "altKey" | "metaKey"]: Modifier;
 };
 
-export const getDecimalPlaces = (multiplier: number): number => {
-  if (multiplier >= 1) return 0;
-  return Math.abs(Math.floor(Math.log10(multiplier)));
+export const defaultModifiers: Modifiers = {
+  default: { multiplier: 1, sensitivity: 1 },
+  ctrlKey: { multiplier: 1, sensitivity: 1 },
+  shiftKey: { multiplier: 10, sensitivity: 0.5 },
+  metaKey: { multiplier: 1, sensitivity: 1 },
+  altKey: { multiplier: 1, sensitivity: 1 },
 };
 
-export const formatNumber = (value: number, decimals: number): string => {
+export function formatNumber(value: number, decimals: number): string {
   return new Intl.NumberFormat("en-US", {
     // if multiplier is 0.1, always show 1 decimal
     minimumFractionDigits: decimals,
@@ -23,4 +23,9 @@ export const formatNumber = (value: number, decimals: number): string => {
     maximumFractionDigits: Math.max(6, decimals),
     useGrouping: false,
   }).format(value);
-};
+}
+
+export function getDecimalPlaces(multiplier: number): number {
+  if (multiplier >= 1) return 0;
+  return Math.abs(Math.floor(Math.log10(multiplier)));
+}
